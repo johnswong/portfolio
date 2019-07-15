8ce300ebc3f6fc85d45082b9bba721026625c81e
Johnson Wong <johnsonwong@Johnsons-MBP.hsd1.ca.comcast.net>
2019-07-15 00:31:27 -0700

    finished portfolio app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Sidebar from './components/sidebar'
+import Introduction from './components/introduction'
+import About from './components/about'
+import Experience from './components/experience'
+import Education from './components/education'
+import Projects from './components/projects'
+import Skills from './components/skills'
+import Contact from './components/contact'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div id="colorlib-page">
+        <div id="container-wrap">
+          <Sidebar></Sidebar>
+          <div id="colorlib-main">
+            <Introduction></Introduction>
+            <About></About>
+            <Skills></Skills>
+            <Experience></Experience>
+            <Projects></Projects>
+            <Education></Education>
+            <Contact></Contact>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/experience.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+export default class Experience extends Component {
+    render() {
+        return (
+            <div>
+                <section className="colorlib-experience" data-section="experience">
+                    <div className="colorlib-narrow-content">
+                        <div className="row">
+                            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                                <span className="heading-meta">experience</span>
+                                <h2 className="colorlib-heading animate-box">Work Experience</h2>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="timeline-centered">
+                                    <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                                        <div className="timeline-entry-inner">
+                                            <div className="timeline-icon color-3">
+                                                <i className="icon-pen2" />
+                                            </div>
+                                            <div className="timeline-label">
+                                                <h2>Technical Specialist at Apple Inc. <span>Oct 2018 - present</span></h2>
+                                                <p><li>Troubleshoot mobile devices such as iPhone, iPad and Apple Watch, as well as iCloud services</li>
+                                                <li>Present options and solutions for customers in order for them to make an informed decision</li>
+                                                <li>Multi-task by attending to different customers in a timely manner</li>
+                                                <li>Assist peers with troubleshooting customer devices</li>
+                                                <li>Share feedback amongst peers to improve external and internal customer experience</li></p>
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                                        <div className="timeline-entry-inner">
+                                            <div className="timeline-icon color-4">
+                                                <i className="icon-pen2" />
+                                            </div>
+                                            <div className="timeline-label">
+                                                <h2>DevOps/IT Intern at Jobvite <span>Jun 2018 - Aug 2018</span></h2>
+                                                <p><li>Responded to help desk tickets that involved office-related technical issues in a timely manner, and troubleshooted problems hands-on when needed </li>
+                                                <li>Configured computers for new hires according to company needs and standards </li>
+                                                <li>Helped migrate a local server, where builds for the mobile platform took place, to the cloud, and insured specific build tools were installed and running properly </li>
+                                                <li>Managed the company’s active directory by organizing and updating users and groups </li></p>
+                                            </div>
+                                        </div>
+                                    </article>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
