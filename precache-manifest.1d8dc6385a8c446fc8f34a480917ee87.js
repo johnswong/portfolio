434d6bd66944d87e5c3d1dbb5fe1aa3fdef4dd1f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-31 16:51:03 +0000

    Deploy johnswong14/johnswong14.github.io-portfolio to github.com/johnswong14/johnswong14.github.io-portfolio.git:gh-pages

---
+++ precache-manifest.1d8dc6385a8c446fc8f34a480917ee87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838ca4b14b0b8d4e2d4bd8c20aae5a81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3de81154980bc10592f2",
+    "url": "/static/css/main.1c1e5eaa.chunk.css"
+  },
+  {
+    "revision": "7c2623ebbf7ec2b5a83e",
+    "url": "/static/js/2.6de8b587.chunk.js"
+  },
+  {
+    "revision": "3de81154980bc10592f2",
+    "url": "/static/js/main.635a8e2b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
